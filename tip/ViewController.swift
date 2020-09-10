6cf991defa598bb137faf7d60204858acb182c58
NickTralongo <30412251+NickTralongo@users.noreply.github.com>
2020-09-09 23:47:30 -0700

    Update ViewController.swift

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -15,9 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
 
 
