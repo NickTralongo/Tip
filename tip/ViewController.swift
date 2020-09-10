65e7008fe6f8feee549cecc4cd67fe74f28b60ff
Nick Tralongo <ntralongo@csumb.edu>
2020-09-09 21:39:46 -0700

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Nick Tralongo on 9/9/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
